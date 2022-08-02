affe6d2bfe0e1e822f073b052ec492c21bd45583
rajivfkhrr <rajiv.fkhrr@gmail.com>
2022-08-02 14:58:38 +0700

    idk

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Preloader
-<script>
-  $(window).on('load', function () {
-    $('.loading').hide();
-  }
-  ) 
-</script>
-
 // highlight current day on opeining hours
 $(document).ready(function() {
 $('.opening-hours li').eq(new Date().getDay()).addClass('today');
